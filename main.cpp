a826a8a93d919623b26999833504a33acb427ec2
Anirudh Dayal <adayal@ucla.edu>
2015-03-03 17:46:25 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "glut.h"
+#include "GameController.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+const string assetDirectory = "/Users/adayal/Desktop/BoulderBlast/Assets";
+
+class GameWorld;
+GameWorld* createStudentWorld(string assetDir = "");
+
+int main(int argc, char* argv[])
+{
+	{
+		string path = assetDirectory;
+		if (!path.empty())
+			path += '/';
+		ifstream ifs(path + "level00.dat");
+		if (!ifs)
+		{
+			cout << "Cannot find level00.dat in ";
+			cout << (assetDirectory.empty() ? "current directory"
+											: assetDirectory) << endl;
+			return 1;
+		}
+	}
+    glutInit(&argc, argv);
+    srand(static_cast<unsigned int>(time(nullptr)));
+    GameWorld* gw = createStudentWorld(assetDirectory);
+    Game().run(gw, "Boulder Blast");
+}
